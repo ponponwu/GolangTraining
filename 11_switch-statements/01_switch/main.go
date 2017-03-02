d2db15d966e1d696eb74112ea3366b6f470610b3
Vincent <sheep0406@hotmail.com>
2017-03-02 18:16:52 +0800

    Add practice 11 switch

---
+++ 11_switch-statements/01_switch/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	switch "Mhi" {
+	case "Daniel":
+		fmt.Println("Wassup Daniel")
+	case "Medhi":
+		fmt.Println("fuck Medhi")
+	case "Jenny":
+		fmt.Println("Have u no friends?")
+	}
+}
---
+++ 11_switch-statements/04_no-expression/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	myFriendsName := "Ma"
+
+	switch {
+	case len(myFriendsName) == 2:
+		fmt.Println("Wassup my friend with name of length 2")
+	case myFriendsName == "Tim":
+		fmt.Println("Wassup Tim")
+	case myFriendsName == "Jenny":
+		fmt.Println("Wassup Jenny")
+	case myFriendsName == "Marcus", myFriendsName == "Medhi":
+		fmt.Println("Your name is Marcus or Medhi")
+	case myFriendsName == "Julian":
+		fmt.Println("Wassup Julian")
+	case myFriendsName == "Sushant":
+		fmt.Println("Wassup Sushant")
+	default:
+		fmt.Println("nothing matched: this is default")
+	}
+}
